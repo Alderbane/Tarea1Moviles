464e98f646f1de0022b76b0860505b1d7e6816d9
Alderbane <51372365+Alderbane@users.noreply.github.com>
2020-08-26 22:02:41 -0500

    Snackbars added

--- lib/main.dart
+++ lib/main.dart
@@ -24,57 +24,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
   DateTime _d = DateTime.now();
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    var _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       key: _scaffoldKey,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(56),
-        child: Builder(
-          builder: (context1) => AppBar(
-            backgroundColor: Colors.deepPurple[300],
-            title: Text('Click the FAB'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.favorite,
-                  color: _counter % 2 == 0 ? Colors.red : Colors.white,
-                ),
-                onPressed: () {
-                  print("AAAAAAAAA");
-                  _counter++;
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[300],
+        title: Text('Click the FAB'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: _counter % 2 == 0 ? Colors.red : Colors.white,
+            ),
+            onPressed: () {
+              _counter++;
+              _scaffoldKey.currentState
+                ..hideCurrentSnackBar()
+                ..showSnackBar(_counter % 2 == 0
+                    ? SnackBar(
+                        content: Text("Scaffold sin accion"),
+                      )
+                    :SnackBar(
+                        content: Text("Mostrar Fecha"),
+                        action: SnackBarAction(
+                          label: "Fecha",
+                          onPressed: () {
+                            _d = DateTime.now();
+                            showDialog(
+                                context: context,
+                                builder: (_) {
+                                  return AlertDialog(
+                                    title: Text("Dialogo"),
+                                    content: Text("$_d"),
+                                  );
+                                });
+                          },
+                        ),
+                      ));
 
-                  Scaffold.of(context1)
-                    // ..hideCurrentSnackBar()
-                    .showSnackBar(_counter % 2 == 0
-                        ? SnackBar(content: Text("Scaffold sin accion"))
-                        : SnackBar(
-                            content: Text("Mostrar Fecha"),
-                            action: SnackBarAction(
-                              label: "Fecha",
-                              onPressed: () {
-                                _d = DateTime.now();
-                                showDialog(
-                                    context: context,
-                                    builder: (_) {
-                                      return AlertDialog(
-                                        title: Text("Dialogo"),
-                                        content: Text("$_d"),
-                                      );
-                                    });
-                              },
-                            )));
-
-                  setState(() {});
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-              ),
-            ],
+              setState(() {});
+            },
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+          ),
+        ],
       ),
       body: Center(
         child: Container(
